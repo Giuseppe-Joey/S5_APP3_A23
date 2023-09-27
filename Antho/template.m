832122d97d8ec8a892d9097ad8e976491245c993
giuseppe lomonaco <giuseppe.lomonaco@usherbrooke.ca>
2023-09-27 09:06:05 -0400

    adding jean and antho folder

---
+++ Antho/template.m
@@ -0,0 +1,21 @@
+%%  S5 - APP3 - PROBLEMATIQUE - XXXXXX_XXXXXXX_XXXXXXX.M
+%   Auteur:     Giuseppe Lomonaco
+%   CIP:        LOMG2301
+%   Auteur:     Jean Dodie Ombeni
+%   CIP:        OMBJ2301
+%   Auteur:     Anthony Royer
+%   CIP:        ROYA2019
+
+%   Date de creation:                       26-Septembre-2023
+%   Date de derniere modification:          26-Septembre-2023
+
+%   DESCRIPTION:    fffffffffffffffffffffffffffffffffffffffffffffffffffff
+%                   ggggggggggggggggggggggggggggggggggggggggggggggggggggg
+
+clc
+close all
+clear 
+
+
+
+
