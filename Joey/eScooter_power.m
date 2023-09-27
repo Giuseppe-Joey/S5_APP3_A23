e226784425e1d4aab72471b0b738be4b29ff3783
giuseppe lomonaco <giuseppe.lomonaco@usherbrooke.ca>
2023-09-27 11:14:13 -0400

    labo1: exercice 1 et 2 finished

---
+++ Joey/eScooter_power.m
@@ -0,0 +1,17 @@
+%%  ESCOOTER_POWER.M
+%   Auteur:     Giuseppe Lomonaco
+%   CIP:        LOMG2301
+
+%   Date de creation:                       26-Septembre-2023
+%   Date de derniere modification:          26-Septembre-2023
+
+%   DESCRIPTION:    fffffffffffffffffffffffffffffffffffffffffffffffffffff
+%                   ggggggggggggggggggggggggggggggggggggggggggggggggggggg
+
+clc
+close all
+clear 
+
+
+
+Fapp = 617.75;          % Force applique en Newtons
